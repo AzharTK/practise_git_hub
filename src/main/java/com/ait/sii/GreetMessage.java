35a288276a318300d8e2c7615289b97c5d00a280
azhartk <mohammadazhartk@gmail.com>
2024-01-09 01:14:07 +0530

    initial commit

---
+++ src/main/java/com/ait/sii/GreetMessage.java
@@ -0,0 +1,21 @@
+package com.ait.sii;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Greet" )
+public class GreetMessage extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		PrintWriter writer = response.getWriter();
+		writer.append(name + ", Good Evening..!!");
+	}
+
+
+}
---
+++ src/main/webapp/welcome.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Greeting message</title>
+</head>
+<body>
+	<h1>welcome to our page</h1>
+	
+	<form action="Greet">
+		Enter Name : <input type="text"  name = "name"/> </br>
+					 <input type="Submit"  value="Submit"/>
+	</form>
+</body>
+</html>
